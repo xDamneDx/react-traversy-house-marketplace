320dcbedd5860aa8d18055f02f5b63b22e1398c8
xDamneDx <xdamnedx@gmail.com>
2022-06-14 10:56:08 +0300

    Added signing in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -16,6 +17,26 @@ function SignIn() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="pageContainer">
@@ -23,7 +44,7 @@ function SignIn() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               className="emailInput"
@@ -31,6 +52,7 @@ function SignIn() {
               id="email"
               value={email}
               onChange={onChange}
+              autoComplete="off"
             />
             <div className="passwordInputDiv">
               <input
@@ -40,6 +62,7 @@ function SignIn() {
                 id="password"
                 value={password}
                 onChange={onChange}
+                autoComplete="off"
               />
               <img
                 src={visibilityIcon}
