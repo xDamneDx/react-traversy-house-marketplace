abbaa13988baa38d390a92c8a6c80f8e4ea7e99b
xDamneDx <xdamnedx@gmail.com>
2022-06-14 11:53:28 +0300

    Added logging out

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import { getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 function Profile() {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;
