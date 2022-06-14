e1a390d89d0094c5197c155d6ad2dfce71c693fc
xDamneDx <xdamnedx@gmail.com>
2022-06-14 13:18:51 +0300

    Added PrivateRoute

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from "./pages/SignUp";
 
 // Components:
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -20,7 +21,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
