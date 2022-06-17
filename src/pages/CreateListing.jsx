f056b6d1e9c2d76e443d8996a2a7276f698205ec
xDamneDx <xdamnedx@gmail.com>
2022-06-17 16:37:59 +0300

    Fixed problem w/ address

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -112,7 +112,6 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
 
     // Store image in firebase:
@@ -168,9 +167,9 @@ function CreateListing() {
     };
 
     // formDataCopy cleanup:
+    formDataCopy.location = address;
     delete formDataCopy.images;
     delete formDataCopy.address;
-    location && (formDataCopy.location = location);
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
     const docRef = await addDoc(collection(db, "listings"), formDataCopy);
