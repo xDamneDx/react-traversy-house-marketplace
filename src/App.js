e54add5c7a3f7aab4733f8bc6c02641229f527b4
xDamneDx <xdamnedx@gmail.com>
2022-06-20 10:22:14 +0300

    Added Listing component w/ route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 
 // Components:
 import Navbar from "./components/Navbar";
@@ -24,6 +25,10 @@ function App() {
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/category/:categoryName" element={<Category />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
