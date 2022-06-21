7c1f681c7a0fd5bfae381a384b963fca8807fe98
xDamneDx <xdamnedx@gmail.com>
2022-06-21 11:29:34 +0300

    Created Slider component

---
+++ src/components/Slider.jsx
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { db } from "../firebase.config";
+
+// Components:
+import Spinner from "./Spinner";
+
+// Swiper:
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+function Slider() {
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listingsRef = collection(db, "listings");
+      const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
+      const querySnap = await getDocs(q);
+
+      const listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings(listings);
+      setLoading(false);
+    };
+
+    fetchListings();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    listings && (
+      <>
+        <p className="exploreHeading">Recommended</p>
+
+        <Swiper
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          className="swiper-container"
+        >
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                className="swiperSlideDiv"
+                style={{
+                  background: `url(${data.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+              >
+                <p className="swiperSlideText">{data.name}</p>
+                <p className="swiperSlidePrice">
+                  ${data.discountedPrice ?? data.regularPrice}
+                  {data.type === "rent" && " / month"}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  );
+}
+
+export default Slider;
