cf13aa3c6824b52c0dab31d3008b09373140dd4e
xDamneDx <xdamnedx@gmail.com>
2022-06-21 13:08:01 +0300

    Added EditListing component w/ route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
 import Listing from "./pages/Listing";
 import Contact from "./pages/Contact";
 
@@ -37,6 +38,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/edit-listing/:listingId" element={<EditListing />} />
           <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
