e696d32169813379e28eb9efc1435657d42d65de
xDamneDx <xdamnedx@gmail.com>
2022-06-21 11:29:56 +0300

    Added Slider component

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
 import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
+// Components:
+import Slider from "../components/Slider";
+
 function Explore() {
   return (
     <div className="explore">
@@ -11,7 +14,7 @@ function Explore() {
         <p className="pageHeader">Explore</p>
       </header>
       <main>
-        {/* Slider */}
+        <Slider />
 
         <p className="exploreCategoryHeading">Categories</p>
         <div className="exploreCategories">
